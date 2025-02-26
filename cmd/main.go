a9cc797a5ae0edc737bf572081b987dc0fbf60ad
Arsenii Trutnev <162054385+1strewave@users.noreply.github.com>
2025-02-26 17:41:14 +0000

    Added some more features

--- cmd/main.go
+++ cmd/main.go
@@ -1,25 +1,46 @@
 package main
 
 import (
-  "github.com/gin-gonic/gin"
-  "net/http"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
 )
 
 type User struct {
-  ID int `json:"id"`
-  Name string `json:"name"`
-  Balance float64 `json:"balance"`
-  isBlocked bool `json:"isBlocked"`
+	ID        int     `json:"id"`
+	Name      string  `json:"name"`
+	Balance   float64 `json:"balance"`
+	IsBlocked bool    `json:"isBlocked"`
 }
 
 var users []User
 
 func main() {
-  r := gin.Default()
+	r := gin.Default()
 
-  r.GET("/users", func(c *gin.Context){
-    c.JSON(http.StatusOK, users)
-  })
+	r.GET("/users", func(c *gin.Context) {
+		c.JSON(http.StatusOK, users)
+	})
 
-  r.Run(":8080")
+	r.GET("/users/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		i, err := strconv.Atoi(id)
+		if err != nil {
+			fmt.Println("Failed to convert id(string) into integer")
+		}
+		for _, user := range users {
+			if user.ID == i {
+				c.JSON(http.StatusOK, user)
+				return
+			}
+		}
+	})
+
+	r.POST("/users", func(c *gin.Context) {
+
+	})
+
+	r.Run(":8080")
 }
